f76cb089b6d06599e6512faafc00c91f534e1205
Hariprasath <balaprasad24@gmail.com>
2020-07-12 12:52:06 +0530

    Created spring boot project using spring initalizr

---
+++ order-item-service/src/test/java/in/hp/java/orderitemservice/OrderItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hp.java.orderitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
