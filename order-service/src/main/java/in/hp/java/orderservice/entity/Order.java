206d09fc9b5f9cfed362245eeef0eb84b598ae51
Hariprasath <balaprasad24@gmail.com>
2020-07-15 17:28:11 +0530

    Added lombok to reduce BPC and increase readability

--- order-service/src/main/java/in/hp/java/orderservice/entity/Order.java
+++ order-service/src/main/java/in/hp/java/orderservice/entity/Order.java
@@ -1,5 +1,8 @@
 package in.hp.java.orderservice.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -8,6 +11,8 @@ import java.sql.Date;
 
 @Entity
 @Table(name = "ORDER_TABLE")
+@Data
+@NoArgsConstructor
 public class Order {
 
     @Id
@@ -18,55 +23,4 @@ public class Order {
     private String shippingAddress;
     private Double total;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public String getShippingAddress() {
-        return shippingAddress;
-    }
-
-    public void setShippingAddress(String shippingAddress) {
-        this.shippingAddress = shippingAddress;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Order{");
-        sb.append("id=").append(id);
-        sb.append(", customerName='").append(customerName).append('\'');
-        sb.append(", orderDate=").append(orderDate);
-        sb.append(", shippingAddress='").append(shippingAddress).append('\'');
-        sb.append(", total=").append(total);
-        sb.append('}');
-        return sb.toString();
-    }
 }
